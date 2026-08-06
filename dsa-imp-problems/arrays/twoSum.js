5eee7e57dce08e083b89a69f5c07b602198d7c3d
Sudharsan <sudharsanganapathy24@gmail.com>
2026-08-06 14:17:01 +0530

    Day 004: Solve Two Sum using brute force approach

---
+++ dsa-imp-problems/arrays/twoSum.js
@@ -0,0 +1,13 @@
+function twoSum(nums, target){
+
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i]+nums[j]===target){
+                return [i, j];
+            }
+        }
+    }
+    return null;
+}
+
+console.log(twoSum([2, 5, 4, 7], 9));
