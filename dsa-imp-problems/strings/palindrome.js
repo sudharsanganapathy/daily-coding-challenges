e270dd9e9081646e903ab4aac2fd6fca7d26046a
Sudharsan <sudharsanganapathy24@gmail.com>
2026-08-07 13:07:36 +0530

    Day 005: Solve palindrome checker using two pointers

---
+++ dsa-imp-problems/strings/palindrome.js
@@ -0,0 +1,21 @@
+function isPalindrome(str){
+
+    let i=0;
+    let j=str.length-1;
+
+    while(i<j){
+        if(str[i]!==str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+    return true;
+
+}
+
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("hello"));
+
