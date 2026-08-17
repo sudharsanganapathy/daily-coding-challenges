12173625a28cbb1d37a087d1a3a19ae0c9586534
Sudharsan <sudharsanganapathy24@gmail.com>
2026-08-17 12:11:44 +0530

    Day 015: Implement email masking

--- front-end-imp-topics/emailMasking.js
+++ front-end-imp-topics/emailMasking.js
@@ -1,3 +1,12 @@
+// Approach:
+// 1. Split the email using "@".
+// 2. Store the username and domain separately.
+// 3. Keep the first character of the username visible.
+// 4. Replace the remaining username characters with "*".
+// 5. Add "@" and the original domain.
+// 6. Return the masked email.
+
+
 function emailMasking(email){
 
     const[username, domain] = email.split("@");
