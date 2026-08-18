d068caa303cd503c0124918315efb82a685edb25
Sudharsan <sudharsanganapathy24@gmail.com>
2026-08-18 12:59:25 +0530

    Day 016: Solve 3Sum using brute force

--- dsa-imp-problems/arrays/threeSum.js
+++ dsa-imp-problems/arrays/threeSum.js
@@ -1,3 +1,15 @@
+// Approach:
+// 1. Create an empty result array to store valid triplets.
+// 2. Use the first loop to select the first element.
+// 3. Use the second loop to select the second element after the first.
+// 4. Use the third loop to select the third element after the second.
+// 5. Calculate the sum of the three selected elements.
+// 6. If the sum is 0, store the three elements as a triplet.
+// 7. Continue checking all possible combinations of three elements.
+// 8. Return all valid triplets.
+
+
+
 function threeSum(nums){
 
     let result = [];
