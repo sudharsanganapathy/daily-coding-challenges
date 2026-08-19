b24217262fd4eb697c2e268a42f8d1213011f12e
Sudharsan <sudharsanganapathy24@gmail.com>
2026-08-19 21:10:22 +0530

    Day 017: Find kth largest element using sorting

--- dsa-imp-problems/arrays/findKthLargestElement.js
+++ dsa-imp-problems/arrays/findKthLargestElement.js
@@ -1,3 +1,8 @@
+// Approach:
+// 1. Sort the array in descending order.
+// 2. The largest element will be at index 0.
+// 3. Since arrays use zero-based indexing, the kth largest element is at index k - 1.
+// 4. Return arr[k - 1].
 
 
 function kthLargestElement(arr, k){
