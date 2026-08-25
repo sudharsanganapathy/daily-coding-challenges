560e0fecc89e198fe72e77ddb11ccf574219a51e
Sudharsan <sudharsanganapathy24@gmail.com>
2026-08-25 22:32:50 +0530

    Count words in a string using split

---
+++ dsa-imp-problems/strings/countWords.js
@@ -0,0 +1,19 @@
+// Approach:
+// 1. Split the string into an array of words using a space.
+// 2. Initialize count as 0.
+// 3. Traverse the array of words.
+// 4. Increment count for every word.
+// 5. Return/print the total word count.
+
+
+let str = "I  love  to   solve    problem";
+
+let strUpdated = str.trim().split(/\s+/);
+
+let count = 0
+
+for(let i=0; i<strUpdated.length; i++){
+    count++;
+}
+
+console.log(count);
