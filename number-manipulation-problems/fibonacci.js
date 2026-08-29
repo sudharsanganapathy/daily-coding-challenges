eb0ac6e306b6dd47ea32fa1ef7b35240157326c0
Sudharsan <sudharsanganapathy24@gmail.com>
2026-08-29 14:57:22 +0530

    Day 027: Generate Fibonacci series using iteration

--- number-manipulation-problems/fibonacci.js
+++ number-manipulation-problems/fibonacci.js
@@ -1,3 +1,13 @@
+// Approach:
+// 1. Initialize the first two Fibonacci numbers as a = 0 and b = 1.
+// 2. Print the first two numbers.
+// 3. Start the loop from index 2 because the first two numbers are already handled.
+// 4. Calculate the next number by adding a and b.
+// 5. Print the next Fibonacci number.
+// 6. Move a to b and b to next for the next iteration.
+// 7. Continue until n Fibonacci numbers are generated.
+
+
 function fibonacci(n){
 
     let a = 0;
