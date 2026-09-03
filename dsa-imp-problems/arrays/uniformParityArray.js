497f5abb9f4bcd40010267acd2d6ef060a880149
Sudharsan <sudharsanganapathy24@gmail.com>
2026-09-03 16:23:09 +0530

    Day 030: Solved Construct Uniform Parity Array I

---
+++ dsa-imp-problems/arrays/uniformParityArray.js
@@ -0,0 +1,8 @@
+function uniformParityArray(nums1){
+    return true;
+}
+
+console.log(uniformParityArray([1, 3]));
+console.log(uniformParityArray([2, 4, 6, 8]));
+console.log(uniformParityArray([1, 3, 5, 7]));
+console.log(uniformParityArray([2, 3, 6, 9]));
