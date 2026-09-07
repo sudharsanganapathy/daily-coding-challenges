3cd97cfa976454391bbc66c353e13b07b9da8858
Sudharsan <sudharsanganapathy24@gmail.com>
2026-09-07 16:28:18 +0530

    Solved Product of Array Except Self

--- dsa-imp-problems/arrays/productOfArrayExceptSelf.js
+++ dsa-imp-problems/arrays/productOfArrayExceptSelf.js
@@ -1,3 +1,14 @@
+// Create a function that accepts the input array
+// Create an empty array to store the final products
+// Traverse through each element of the array
+// Initialize the product as 1 for the current element
+// Traverse through the entire array again
+// Skip the current element using i !== j
+// Multiply all other elements with the product
+// Add the calculated product to the result array
+// Return the result array.
+
+
 function productOfArrayExceptSelf(nums){
 
     let result = [];
